657399b44a71f9b8f30ad9e8bcdd07c30cbede97
chad-tung <chad.tung@gmail.com>
2018-01-15 16:48:36 +0000

    Altered some tests to match changes

--- src/specs/scale_spec.js
+++ src/specs/scale_spec.js
@@ -8,15 +8,15 @@ describe('key tests', function() {
   });
 
   it('should have a chromatic', function() {
-    assert.deepStrictEqual(scale.chromatic, ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"]);
+    assert.deepStrictEqual(scale.chromatic, ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]);
   });
 
   it('should be able to alter the chromatic start point', function() {
-    scale.setChromatic("C");
-    assert.deepStrictEqual(scale.chromatic, ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"])
+    scale.setChromatic("A");
+    assert.deepStrictEqual(scale.chromatic, ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"])
   });
 
-  it('should be able to set up major scale', function() {
-    scale.setChromatic("C");
-  })
+  // it('should be able to set up major scale', function() {
+  //   scale.setChromatic("C");
+  // })
 })
