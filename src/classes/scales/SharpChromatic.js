c6de82eb5e6aca93e6c9330ca29fc2160ed4fd8c
chad-tung <chad.tung@gmail.com>
2018-01-16 17:51:04 +0000

    Got tests to pass

--- src/classes/scales/SharpChromatic.js
+++ src/classes/scales/SharpChromatic.js
@@ -7,15 +7,37 @@ for (item of letters) {
   validLetters.push(item + "b");
 }
 
-let defaultChromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]
+let default_chromatic = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]
 
 
 let SharpChromatic = function(note) {
-  this.scale = [];
+  this.scale = this.setScale(note);
 }
 
 SharpChromatic.prototype.setScale = function (note) {
-  
+  let relative_flats = ["Db", "Eb", "Gb", "Ab", "Bb"];
+  let relative_sharps = ["C#", "D#", "F#", "G#", "A#"];
+
+  let sharp_key = note;
+
+  if (note && validLetters.includes(note)) {
+
+    if (relative_flats.includes(note)) {
+      sharp_key = relative_sharps[relative_flats.indexOf(note)]
+    }
+
+    old_chromatic = default_chromatic.slice(0, 12);
+
+    let keyIndex = old_chromatic.indexOf(sharp_key);
+    let end_notes = old_chromatic.splice(0, keyIndex);
+    let new_chromatic = old_chromatic.concat(end_notes);
+    new_chromatic.push(sharp_key);
+    return new_chromatic;
+  } else {
+    return default_chromatic;
+  }
+
+
 };
 
 module.exports = SharpChromatic;
---
+++ src/specs/sharpChromatic_spec.js
@@ -0,0 +1,25 @@
+let assert = require('assert');
+let SharpChromatic = require('../classes/scales/SharpChromatic.js');
+
+describe('sharp chromatic scale tests', function() {
+
+  it('should have a default chromatic starting with C', function() {
+    let c_chrom = new SharpChromatic();
+    assert.deepStrictEqual(c_chrom.scale, ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]);
+  });
+
+  it('should set a chromatic scale starting with A as its scale when initialised with argument', function() {
+    let a_chrom = new SharpChromatic("A");
+    assert.deepStrictEqual(a_chrom.scale, ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A"]);
+  });
+
+  it('should handle flat accidentals', function() {
+    let db_chrom = new SharpChromatic("Db");
+    assert.deepStrictEqual(db_chrom.scale, ["C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C", "C#"]);
+  });
+
+  it('should set C as the chromatic for incompatible inputs', function() {
+    let z_chrom = new SharpChromatic("h");
+    assert.deepStrictEqual(z_chrom.scale, ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]);
+  });
+})
