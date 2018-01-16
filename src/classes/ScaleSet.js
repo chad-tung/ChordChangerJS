f11a68a8eddd001209d9cc3d57207054cfb4f749
chad-tung <chad.tung@gmail.com>
2018-01-16 04:19:40 +0000

    Removed unneccessary commented out code

--- src/classes/ScaleSet.js
+++ src/classes/ScaleSet.js
@@ -25,7 +25,6 @@ ScaleSet.prototype = {
     }
 
     let keys = [sharp_key, flat_key];
-
     let changed_chromatic = this.sharp_chromatic;
     let changed_flat_chromatic = this.flat_chromatic;
     let chromatics = [changed_chromatic, changed_flat_chromatic];
@@ -44,33 +43,6 @@ ScaleSet.prototype = {
     this.sharp_chromatic = new_chromatics[0];
     this.flat_chromatic = new_chromatics[1];
 
-
-    // this.sharp_chromatic = new_chromatic;
-
-// This function below works only for white keys.
-    // let changed_chromatic = this.sharp_chromatic;
-    // let changed_flat_chromatic = this.flat_chromatic;
-    // let chromatics = [changed_chromatic, changed_flat_chromatic];
-    // let new_chromatics = [];
-    // for (var old_chromatic of chromatics) {
-    //   old_chromatic.pop();
-    //   let keyIndex = old_chromatic.indexOf(note);
-    //   let end_notes = old_chromatic.splice(0, keyIndex);
-    //   new_chromatic = old_chromatic.concat(end_notes);
-    //   new_chromatic.push(note);
-    //   new_chromatics.push(new_chromatic);
-    // }
-    // this.sharp_chromatic = new_chromatics[0];
-    // this.flat_chromatic = new_chromatics[1];
-
-
-    // let old_chromatic = this.chromatic;
-    // old_chromatic.pop();
-    // let keyIndex = old_chromatic.indexOf(note);
-    // let end_notes = old_chromatic.splice(0, keyIndex);
-    // new_chromatic = old_chromatic.concat(end_notes);
-    // new_chromatic.push(note);
-    // this.chromatic = new_chromatic;
   },
 
   setMajor: function() {
