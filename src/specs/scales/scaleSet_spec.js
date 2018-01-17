c9022c7af6f57c95c434e6a73822be5352f6a807
chad-tung <chad.tung@gmail.com>
2018-01-17 18:09:01 +0000

    Started testing G scaleset

--- src/specs/scales/scaleSet_spec.js
+++ src/specs/scales/scaleSet_spec.js
@@ -4,7 +4,13 @@ let ScaleSet = require('../../classes/ScaleSet.js');
 describe('ScaleSet test', function() {
   beforeEach(function() {
     def_scaleset = new ScaleSet();
+    g_scaleset = new ScaleSet("G");
+    c_sharp_scaleset = new ScaleSet("C#");
   });
+
+  it('should default setup with a C tonic', function() {
+    assert.strictEqual(def_scaleset.tonic, "C");
+  })
 
   it('should default setup with a C major scale', function() {
     assert.deepStrictEqual(def_scaleset.major, ["C", "D", "E", "F", "G", "A", "B", "C"]);
@@ -17,4 +23,12 @@ describe('ScaleSet test', function() {
   it('should default setup with a C chromatic', function() {
     assert.deepStrictEqual(def_scaleset.sharp_chromatic, ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C"]);
   });
+
+  it('should default setup with a C chromatic with flats', function() {
+    assert.deepStrictEqual(def_scaleset.flat_chromatic, ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C"]);
+  });
+
+  it('should G initialise with a G tonic', function() {
+    assert.strictEqual(g_scaleset.tonic, "G");
+  })
 })
