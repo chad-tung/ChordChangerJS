4fd6bada01952878d5bbf670893525e22383b0f6
chad-tung <chad.tung@gmail.com>
2018-04-30 16:26:09 +0800

    Refactrored minor scale and sharp chromatic so far

--- src/classes/scales/MinorScale.js
+++ src/classes/scales/MinorScale.js
@@ -1,46 +1,49 @@
 let _ = require('lodash');
 let SharpChromatic = require('./SharpChromatic.js');
 let FlatChromatic = require('./FlatChromatic.js');
+let Scale = require('./Scale.js');
 
 
-let MinorScale = function(note) {
-  this.scale = this.setScale(note);
-}
-
-MinorScale.prototype.setScale = function(note) {
-  let e_flat_exception = "Eb";
-  let flat_scales = ["D", "G", "C", "F", "Bb"];
-  let sharp_scales = ["G#", "C#", "F#", "B", "E", "A"];
-
-  let exceptions = ["Db", "D#", "Gb", "Ab", "A#"];
-  let replacements = ["C#", "Eb", "F#", "G#", "Bb"];
-
-  let chromatic_scale = [];
-  let minor_scale = [];
-  let keynote = ""
-
-  if (note) {
-    keynote = (exceptions.includes(note)) ?
-      replacements[exceptions.indexOf(note)] : note;
-  } else {
-    keynote = "C"
+class MinorScale extends Scale {
+  constructor(note) {
+    super(note);
+    this.scale = this.setScale(note);
   }
 
-  if (keynote == e_flat_exception) {
-    minor_scale = ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db", "Eb"];
-  } else {
-    chromatic_scale = (flat_scales.includes(keynote)) ?
-      new FlatChromatic(keynote).scale : new SharpChromatic(keynote).scale
+  setScale(note) {
+    let e_flat_exception = "Eb";
+    let flat_scales = ["D", "G", "C", "F", "Bb"];
+    let sharp_scales = ["G#", "C#", "F#", "B", "E", "A"];
 
-    let minorArr = [0, 2, 3, 5, 7, 8, 10, 12];
-    minor_scale = _.at(chromatic_scale, minorArr);
+    let exceptions = ["Db", "D#", "Gb", "Ab", "A#"];
+    let replacements = ["C#", "Eb", "F#", "G#", "Bb"];
+
+    let chromatic_scale = [];
+    let minor_scale = [];
+    let keynote = ""
+
+    if (note) {
+      keynote = (exceptions.includes(note)) ?
+        replacements[exceptions.indexOf(note)] : note;
+    } else {
+      keynote = "C"
+    }
+
+    if (keynote == e_flat_exception) {
+      minor_scale = ["Eb", "F", "Gb", "Ab", "Bb", "Cb", "Db", "Eb"];
+    } else {
+      chromatic_scale = (flat_scales.includes(keynote)) ?
+        new FlatChromatic(keynote).scale : new SharpChromatic(keynote).scale
+
+      let minorArr = [0, 2, 3, 5, 7, 8, 10, 12];
+      minor_scale = _.at(chromatic_scale, minorArr);
+    }
+    return minor_scale
+  };
+
+  getScale() {
+    return this.scale;
   }
-// I think this is incorrect, this function should only deal with setting the function, not returning the scale it has set. That should be dealt with by a getter.
-  return minor_scale
-};
-
-MinorScale.prototype.getScale = function() {
-  return this.scale;
 }
 
 module.exports = MinorScale;
