1e1d679acafc5e0de599e7d3dbc0c42fc7b87e38
chad-tung <chad.tung@gmail.com>
2018-04-30 16:33:07 +0800

    Finished refactoring flatchromatic and also made class use const instead of 'let' where possible

--- src/classes/scales/FlatChromatic.js
+++ src/classes/scales/FlatChromatic.js
@@ -1,32 +1,40 @@
-let FlatChromatic = function(note) {
-  this.scale = this.setScale(note);
-}
+const Scale = require('./Scale.js');
 
-FlatChromatic.prototype.setScale = function (note) {
-  let default_chromatic = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C"]
-
-  let relative_flats = ["Db", "Eb", "Gb", "Ab", "Bb"];
-  let relative_sharps = ["C#", "D#", "F#", "G#", "A#"];
-
-  let flat_key = note;
-
-  if (note) {
-
-    if (relative_sharps.includes(note)) {
-      flat_key = relative_flats[relative_sharps.indexOf(note)]
-    }
-
-    old_chromatic = default_chromatic.slice(0, 12);
-
-    let keyIndex = old_chromatic.indexOf(flat_key);
-    let end_notes = old_chromatic.splice(0, keyIndex);
-    let new_chromatic = old_chromatic.concat(end_notes);
-    new_chromatic.push(flat_key);
-    return new_chromatic;
-  } else {
-    return default_chromatic;
+class FlatChromatic extends Scale {
+  constructor(note) {
+    super(note);
+    this.scale = this.setScale(note);
   }
-  
-};
+
+  setScale(note) {
+    const default_chromatic = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B", "C"]
+
+    const relative_flats = ["Db", "Eb", "Gb", "Ab", "Bb"];
+    const relative_sharps = ["C#", "D#", "F#", "G#", "A#"];
+
+    let flat_key = note;
+
+    if (note) {
+
+      if (relative_sharps.includes(note)) {
+        flat_key = relative_flats[relative_sharps.indexOf(note)]
+      }
+
+      let old_chromatic = default_chromatic.slice(0, 12);
+
+      let keyIndex = old_chromatic.indexOf(flat_key);
+      let end_notes = old_chromatic.splice(0, keyIndex);
+      let new_chromatic = old_chromatic.concat(end_notes);
+      new_chromatic.push(flat_key);
+      return new_chromatic;
+    } else {
+      return default_chromatic;
+    }
+  }
+
+  getScale(note) {
+    return this.scale;
+  }
+}
 
 module.exports = FlatChromatic;
