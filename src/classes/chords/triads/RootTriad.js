4d089185d3b3c024439cb6a93b40e0dedcf070ce
chad-tung <chad.tung@gmail.com>
2018-06-09 13:45:34 +0100

    Made some changes and updated npm

--- src/classes/chords/triads/RootTriad.js
+++ src/classes/chords/triads/RootTriad.js
@@ -2,12 +2,12 @@ let ScaleSet = require('../../../ScaleSet.js')
 let Scale = require('../../../Scale.js');
 
 let RootTriad = function(scale, note) {
-  (scale instanceof ScaleSet)this.scale = scale;
+  this.scale = (scale instanceof Scale) ? scale : new Scale();
   this.notes = [];
 }
 
 RootTriad.prototype.setTriad = function(note) {
-
+  this.notes[] =
 }
 
 module.exports = RootTriad;
