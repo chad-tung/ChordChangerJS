289c25d3d85058586022ff650032c2e06cf35287
chad-tung <chad.tung@gmail.com>
2018-06-09 13:50:14 +0100

    Refactored Key into a proper class

--- src/classes/Key.js
+++ src/classes/Key.js
@@ -10,25 +10,27 @@ for (item of letters) {
   validLetters.push(item + "b");
 }
 
-let Key = function(note, major_minor) {
-  this.keynote = null;
-  this.tonality = null;
-  this.scale = [];
-  this.chords = [];
-}
-
-Key.prototype = {
-  setKey: function(targetNote, targetTone) {
-    this.note = targetNote;
-    this.tonality = targetTone;
-  },
-  getKey: function() {
-    return this.note + this.tonality;
-  },
-  setScale: function(note) {
-    let scales = new ScaleSet(note);
-    this.scale = (tonality === "major") ? scales.major : scales.minor;
+class Key {
+  constructor(note, major_minor) {
+    this.keynote = null;
+    this.tonality = null;
+    this.scale = [];
+    this.chords = [];
   }
+
+  setKey(targetNote, targetTone) {
+      this.note = targetNote;
+      this.tonality = targetTone;
+    }
+
+  getKey() {
+      return this.note + this.tonality;
+    }
+
+  setScale(note) {
+      let scales = new ScaleSet(note);
+      this.scale = (tonality === "major") ? scales.major : scales.minor;
+    }
 }
 
 module.exports = Key;
