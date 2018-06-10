e4a175684ed95add5f7f7adbf6abf8462ba5083f
chad-tung <chad.tung@gmail.com>
2018-06-10 12:23:58 +0100

    Fixed validNotes class

--- src/classes/rules/ValidNotes.js
+++ src/classes/rules/ValidNotes.js
@@ -1,23 +1,24 @@
 class ValidNotes
 {
   constructor() {
-    this.setValidNotes()
+    // this.setValidNotes()
+    this.notes = ['A', 'A#', 'Ab', 'B', 'B#', 'Bb', 'C', 'C#', 'Cb', 'D', 'D#', 'Db', 'E', 'E#', 'Eb', 'F', 'F#', 'Fb', 'G', 'G#', 'Gb']
     this.exceptions = ['Cb', 'B#', 'Fb', 'E#']
     this.replacements = ['B', 'C', 'E', 'F']
   }
 
-  setValidNotes() {
-    let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
-    let validLetters = []
-
-    for (let i = 0; i < letters.length; i ++) {
-      validLetters.push(letters[i])
-      validLetters.push(letters[i] + '#')
-      validLetters.push(letters[i] + 'b')
-    }
-
-    this.notes = validLetters
-  }
+  // setValidNotes() {
+  //   let letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+  //   let validLetters = []
+  //
+  //   for (let i = 0; i < letters.length; i ++) {
+  //     validLetters.push(letters[i])
+  //     validLetters.push(letters[i] + '#')
+  //     validLetters.push(letters[i] + 'b')
+  //   }
+  //
+  //   this.notes = validLetters
+  // }
 
   getValidNotes() {
     return this.notes
